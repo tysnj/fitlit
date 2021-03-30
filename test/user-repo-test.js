513181973d743c5135ef456eeaddb44396a26925
kendallha <kendallhaw@gmail.com>
2021-03-29 17:09:12 -0700

    Add tests for user-repo

---
+++ test/user-repo-test.js
@@ -0,0 +1,48 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const dataFile = require("../data/user-test-data");
+const users = dataFile.userTestData;
+const UserRepository = require("../src/user-repo");
+
+describe("UserRepository", function() {
+    let userRepo, users;
+
+    // beforeEach(function() {
+    //   userRepo = new UserRepository(users)
+    // });
+
+    it("should be a function", function() {
+       expect(UserRepository).to.be.a("function"); 
+    });
+
+    it("should be an instance of UserRepository", function() {
+        expect(userRepo).to.be.an.instanceOf(UserRepository);
+    });
+
+    it("should contain Users", function() {
+        expect(userRepo[0].to.be.an.instanceOf(User));
+    });
+
+    it("should be able to retrieve a user's details using their ID", function() {
+        expect(userRepo.getUserInfo(1)).to.deep.equal({
+            "id": 1,
+            "name": "John Doe",
+            "address": "15195 Nakia Tunnel, Spokane WA 19508-5687",
+            "email": "mojo_jojo@hotmail.com",
+            "strideLength": 4.3,
+            "dailyStepGoal": 10000,
+            "friends": [
+              16,
+              4,
+              8
+            ]
+          });
+    });
+
+    it("should be able to calculate the average step goal among users", function() {
+        expect(userRepo.calculateAvgStepGoal()).to.equal(7500);
+    });
+
+
+})
