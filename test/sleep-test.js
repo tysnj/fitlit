365a81675cc603a8f13d9b37cef4dcfd7df527fe
kendallha <kendallhaw@gmail.com>
2021-03-30 13:26:10 -0700

    Add test setup and initial tests for sleep class

--- test/sleep-test.js
+++ test/sleep-test.js
@@ -12,13 +12,36 @@ const User = require("../src/users");
 describe("Sleep", function() {
   let userRepo;
 
-  beforeEach(function() {
-    userRepo = new UserRepository(users);
-    sleep = new Sleep(userRepo.users[0].id)
-  });
+  // beforeEach(function() {
+  //   userRepo = new UserRepository(users);
+  //   sleep = new Sleep(userRepo.users[0].id)
+  // });
 
   it("should be a function", function() {
     expect(Sleep).to.be.a("function");
-  })
+  });
   
+  it("should be an instance of Sleep", function() {
+    expect(sleep).to.be.an.instanceOf(Sleep);
+  });
+
+  it("should contain a single user's sleep data", function() {
+    expect(sleep.userSleep).to.deep.equal([{
+      "userID": 1,
+      "date": "2019/06/15",
+      "hoursSlept": 6.1,
+      "sleepQuality": 2.2
+    }, 
+    {
+      "userID": 1,
+      "date": "2019/06/16",
+      "hoursSlept": 4.1,
+      "sleepQuality": 3.8
+    }])
+  });
+
+
+
+
+
 })
