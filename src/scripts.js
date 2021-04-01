9772cb2f69023d8fc39d5fe0f8546acb4240cfbd
kendallha <kendallhaw@gmail.com>
2021-03-31 19:44:31 -0700

    Initial commit

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+
+// var userID = 1;
+
+//QUERY SELECTOR VARIABLES
+let userName = document.querySelector("#greeting");
+
+//EVENT LISTENERS
+window.addEventListener("load", displayUserData);
+
+function displayUserData() {
+  greetUser();
+}
+
+function greetUser(userID = 1) {
+  let allUsers = new UserRepository(userTestData);
+  let currentUser = allUsers.users.find(user => user.id === userID);
+  userName.innerText = `Hey, ${currentUser.name}`;
+}
+//on page load:
+//date is automatically selected to most recent date in data
+//user class is instantiated
+//user repo is instantiated 
+//
