b2c75b792ad3af9afb88a3d392f68382f20f1791
kendallha <kendallhaw@gmail.com>
2021-03-31 20:45:48 -0700

    Display user info on the dashboard

--- src/scripts.js
+++ src/scripts.js
@@ -1,23 +1,55 @@
-
-// var userID = 1;
+let allUsers = new UserRepository(userData); 
 
 //QUERY SELECTOR VARIABLES
 let userName = document.querySelector("#greeting");
+let userSection = document.querySelector("#userInfoSection");
+let stepGoal = document.querySelector("#stepGoal");
+var ctx = document.getElementById('#userBar').getContext('2d');
+
+//CHART VARS
+var myBarChart = new Chart(ctx, {
+  type: 'horizontalBar',
+  data: data,
+  options: options
+});
 
 //EVENT LISTENERS
 window.addEventListener("load", displayUserData);
 
+function getUserInfo(userID = 1) {
+  return allUsers.users.find(user => user.id === userID);
+}
 function displayUserData() {
   greetUser();
+  displayIDCard();
+  calculateStepGoal();
 }
 
 function greetUser(userID = 1) {
-  let allUsers = new UserRepository(userTestData);
-  let currentUser = allUsers.users.find(user => user.id === userID);
-  userName.innerText = `Hey, ${currentUser.name}`;
+  let currentUser = getUserInfo();
+  userName.innerText = `Hey, ${currentUser.name.split(" ")[0]}`;
 }
-//on page load:
-//date is automatically selected to most recent date in data
-//user class is instantiated
-//user repo is instantiated 
-//
+
+function displayIDCard(userID = 1) {
+  let currentUser = getUserInfo();
+  userSection.innerHTML =        
+  `<div class="left-side">
+    <p class="user-info">ID: ${currentUser.id} </p>
+    <p class="user-info">Name: ${currentUser.name}</p>
+    <p class="user-info">Address: ${currentUser.address}</p>
+  </div>
+  <div class="right-side">
+    <p class="user-info">Email: ${currentUser.email}</p>
+    <p class="user-info">Stride Length: ${currentUser.strideLength}</p>
+    <p class="user-info">Daily Step Goal: ${currentUser.dailyStepGoal}</p>
+    <p class="user-info">Friends: ${currentUser.friends.length}</p>
+  </div>`
+}
+
+function calculateStepGoal(userID = 1) {
+  let currentUser = getUserInfo();
+  let avgStepGoal = (allUsers.users.reduce((acc, user) => acc + user.dailyStepGoal)) / allUsers.users.length;
+}
+
+
+
